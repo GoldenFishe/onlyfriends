1fefde5043e4e1005841efb236faa2fb7a80db14
ag.riazanov <ag.riazanov@gmail.com>
2020-10-22 00:28:34 +0300

    + add some styles and templates;

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './pages/login/login.component';
-import {FormsModule} from "@angular/forms";
-import { ProfileComponent } from './pages/profile/profile.component';
+import {ProfileComponent} from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
